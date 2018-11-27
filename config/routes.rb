8355a4800eb52dbba53284b60a5f4cdcb10a24c5
admin2 <admin2@MacBook-Pro-Vladimir.local>
2018-11-28 00:33:41 +0600

    ^ changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
     root to: redirect('/system/dashboard')
     resource :organization, only: %i[new create]
 
-    scope 'system' do
-      get '/dashboard', to: 'system/dashboard#index'
+    namespace 'system' do
+      resources :dashboard, only: %i[index]
     end
 
   end
