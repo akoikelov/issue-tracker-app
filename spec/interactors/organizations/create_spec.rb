8bca3013ce0138e8d59081ffd73b221a7267e1d4
admin2 <admin2@MacBook-Pro-Vladimir-2.local>
2018-12-03 20:57:16 +0600

    * fix bug with spec

--- spec/interactors/organizations/create_spec.rb
+++ spec/interactors/organizations/create_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-RSpec.describe CreateOrganization, type: :interactor do
+RSpec.describe Organizations::Choose, type: :interactor do
   describe '.call' do
     pending "add some examples to (or delete) #{__FILE__}"
   end
