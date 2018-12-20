d474f717b4a6cbcb055368b84c88d3bdbddfccb9
admin2 <admin2@MacBook-Pro-Vladimir-2.local>
2018-12-20 21:13:52 +0600

    + added token field to Invite model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_151731) do
+ActiveRecord::Schema.define(version: 2018_12_20_151239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_151731) do
     t.date "expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
     t.index ["organization_id"], name: "index_invites_on_organization_id"
   end
 
