cca7747ca6a54b599699c7d6a9fb29463d8bb67e
admin2 <admin2@MacBook-Pro-Vladimir-2.local>
2018-12-30 21:02:42 +0600

    + implemented Invite.gen_token method, created test for it

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,3 +1,18 @@
 class Invite < ApplicationRecord
   belongs_to :organization
+
+  before_create :set_token
+
+  def set_token
+    if token.nil?
+      self.token = gen_token
+    end
+  end
+
+  private
+
+  def gen_token
+    temp = (email + rand(10000000).to_s).split('').shuffle.join
+    Digest::MD5.hexdigest temp
+  end
 end
--- test/models/invite_test.rb
+++ test/models/invite_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class InviteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "gen_token method returns 32-length hash" do
+    invite = Invite.new(email: 'test@test.com')
+    token = invite.instance_eval('gen_token')
+
+    assert_equal 32, token.length
+  end
 end
